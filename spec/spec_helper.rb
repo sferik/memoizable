8959ffa3075ce4fd09adebe8b5358662492bd210
Erik Berlin <sferik@gmail.com>
2026-02-09 06:02:34 -0800

    Add JRuby support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,14 @@ begin
     add_filter "/config"
     add_filter "/spec"
     add_filter "/vendor"
-    enable_coverage :branch
     command_name "spec"
-    minimum_coverage line: 100, branch: 100
+
+    if RUBY_ENGINE == "ruby"
+      enable_coverage :branch
+      minimum_coverage line: 100, branch: 100
+    else
+      minimum_coverage line: 100
+    end
   end
 rescue LoadError
   warn "Warning: simplecov is not installed. Coverage analysis will be skipped."
